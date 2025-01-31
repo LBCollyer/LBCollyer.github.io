ae83b910d746d7a53841ceda3fd9ef1ef7c1d476
LBCollyer <lbcollyer477@gmail.com>
2025-01-31 13:45:20 -0700

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,18 @@
 //javascript
+const darkModeToggle = document.getElementById('darkToggle');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === 'dark') {
+  document.body.classList.add('dark-mode');
+  darkModeToggle.classList.add('dark-mode');
+}
+
+darkModeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  darkModeToggle.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme','light');
+  }
+});
