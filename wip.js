b4664d3f943c66eb3c08fb169e423bfc1828fe22
LBCollyer <lbcollyer477@gmail.com>
2025-04-04 14:36:50 -0600

    Create wip.js

---
+++ wip.js
@@ -0,0 +1,10 @@
+window.my = {};
+require([
+  "esri/layers/FeatureLayer",
+  "esri/renderers/UniqueValueRenderer",
+  "esri/symbols/SimpleMarkerSymbol",
+  "esri/Color",
+  "esri/views/navigation/Navigation"
+], function(FeatureLayer, UniqueValueRenderer, SimpleMarkerSymbol, Color, Navigation) {
+  // ... all your JS goes here exactly as it is ...
+});
