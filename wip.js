d176f6d5f88c6f8e47f8ee37d1f408bc7f325d9d
LBCollyer <lbcollyer477@gmail.com>
2025-04-14 13:50:14 -0600

    Update wip.js

--- wip.js
+++ wip.js
@@ -160,7 +160,7 @@ require([
           my.storyContainer.style.pointerEvents = "none"; // Disable interaction
           my.view.goTo({
             center:[-98, 39],
-            scale: 17000000 
+            scale: 20000000 
           })
           if (my.corFacil) {
             my.corFacil.visible = false;
